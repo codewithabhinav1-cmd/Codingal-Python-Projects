99d634f523a2b2b1136e433ae6cea49f96d8e956
codewithabhinav1-cmd <codewithabhinav1@gmail.com>
2026-06-11 01:07:27 +0530

    push

---
+++ M1/L8/ACP/acp.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Before swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+a, b, c = b, c, a
+
+print("After swapping:")
+print("a =", a, "b =", b, "c =", c)
