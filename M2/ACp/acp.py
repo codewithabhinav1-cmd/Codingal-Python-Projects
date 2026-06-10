3f26e97d37c54aad56b115b98f2d8e073e8fb1bb
codewithabhinav1-cmd <codewithabhinav1@gmail.com>
2026-06-10 20:35:18 +0530

    push

---
+++ M2/ACp/acp.py
@@ -0,0 +1,13 @@
+# Storing birthdays in 5 different variables
+
+birthday1 = "15 January"
+birthday2 = "22 March"
+birthday3 = "10 May"
+birthday4 = "18 August"
+birthday5 = "30 December"
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
